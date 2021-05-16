d06c53f943969e0b2712600d4eed3a6dfd0c1978
liyunlong <573496233@qq.com>
2021-05-16 23:27:29 +0800

    提交numpy的运算

--- deeplearning/NeuralNetwork.py
+++ deeplearning/NeuralNetwork.py
@@ -10,19 +10,28 @@ import numpy as np
 
 class NeuralNetwork:
     '神经网络框架'
-    def __init__(self, deep, weight, rate):
-        self.deep = deep; #深度
-        self.weight = weight; #权重或参数
-        self.rate = rate; #学习率
+    rate = 0.01;
+    def __init__(self, level):
+        # 记录是第几层
+        self.level = level;
 
-    def learn(self):
-        a = np.array(self.deep);
-        for i in range(self.deep):
-            a[i] = np.exp(i) * self.rate;
-            
+    """
+    x：输入x
+    w：训练参数w
+    """    
+    def learn(self, x, w):
+        z = np.dot(w, x);
+        print(z);
+        a = 1 / (1 + np.exp(-z));
         print(a);
-    
-nw = NeuralNetwork(3, 0, 0.01);
 
-nw.learn();
+x = np.arange(1,9).reshape(2,4);
+w = np.array([1,1,2,2]).reshape(2,2);
+print(x);
+print(w);
+        
+nw = NeuralNetwork(1);
+nw.learn(x, w);
+    
+
             
